afe78a2aa18d7beb40910cd51be084c0740d8605
eckim97 <weoncheol1@naver.com>
2024-08-13 19:15:30 +0900

    #45 - JUnit5 테스트에서 불필요한 public 접근자 삭제
    공식문서에 의하면 JUnit5 단위 테스트는
    클래스와 메소드에 `public` 접근자를 필요로 하지 않는다.
    전체 테스트를 검사하여 삭제해줌.

--- src/test/java/org/example/projectboard/controller/AuthControllerTest.java
+++ src/test/java/org/example/projectboard/controller/AuthControllerTest.java
@@ -1,36 +1,49 @@
 package org.example.projectboard.controller;
 
 import org.example.projectboard.config.SecurityConfig;
+import org.example.projectboard.config.TestSecurityConfig;
 import org.example.projectboard.domain.Article;
+import org.example.projectboard.service.ArticleService;
+import org.example.projectboard.service.PaginationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 인증")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(Void.class)
-public class AuthControllerTest {
+class AuthControllerTest {
+
     private final MockMvc mvc;
 
-    public AuthControllerTest(@Autowired MockMvc mvc) {
+    @MockBean private ArticleService articleService;
+    @MockBean private PaginationService paginationService;
+
+    AuthControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
+
     @DisplayName("[view][GET] 로그인 페이지 - 정상 호출")
     @Test
-    public void givenNothing_whenTryingToLogin_thenReturnsLogInView() throws Exception {
-        // given
+    void givenNothing_whenTryingToLogIn_thenReturnsLogInView() throws Exception {
+        // Given
 
-        // when & then
+        // When & Then
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+        then(articleService).shouldHaveNoInteractions();
+        then(paginationService).shouldHaveNoInteractions();
     }
+
 }
