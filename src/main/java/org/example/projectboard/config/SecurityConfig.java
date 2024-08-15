55039082d5527387b580aba6c11b37ba92268d7e
eckim97 <weoncheol1@naver.com>
2024-08-16 05:45:55 +0900

    #54 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/main/java/org/example/projectboard/config/SecurityConfig.java
+++ src/main/java/org/example/projectboard/config/SecurityConfig.java
@@ -1,57 +1,100 @@
     package org.example.projectboard.config;
 
-import org.example.projectboard.dto.UserAccountDto;
-import org.example.projectboard.dto.security.BoardPrincipal;
-import org.example.projectboard.repository.UserAccountRepository;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+    import org.example.projectboard.dto.security.BoardPrincipal;
+    import org.example.projectboard.dto.security.KakaoOAuth2Response;
+    import org.example.projectboard.service.UserAccountService;
+    import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.http.HttpMethod;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.core.userdetails.UserDetailsService;
+    import org.springframework.security.core.userdetails.UsernameNotFoundException;
+    import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+    import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+    import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+    import org.springframework.security.oauth2.core.user.OAuth2User;
+    import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+    import java.util.UUID;
 
+    import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers(
-                                HttpMethod.GET,
-                                "/",
-                                "/articles",
-                                "/articles/search-hashtag"
-                        ).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(withDefaults())
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/")
-                )
-                .build();
+    @Configuration
+    public class SecurityConfig {
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(
+                HttpSecurity http,
+                OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+        ) throws Exception {
+            return http
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                            .requestMatchers(
+                                    HttpMethod.GET,
+                                    "/",
+                                    "/articles",
+                                    "/articles/search-hashtag"
+                            ).permitAll()
+                            .anyRequest().authenticated()
+                    )
+                    .formLogin(withDefaults())
+                    .logout(logout -> logout.logoutSuccessUrl("/"))
+                    .oauth2Login(oAuth -> oAuth
+                            .userInfoEndpoint(userInfo -> userInfo
+                                    .userService(oAuth2UserService)
+                            )
+                    )
+                    .build();
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService(UserAccountService userAccountService) {
+            return username -> userAccountService
+                    .searchUser(username)
+                    .map(BoardPrincipal::from)
+                    .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
+        }
+
+        @Bean
+        public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+                UserAccountService userAccountService,
+                PasswordEncoder passwordEncoder
+        ) {
+            final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+            return userRequest -> {
+                OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+                KakaoOAuth2Response kakaoResponse = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+                String registrationId = userRequest.getClientRegistration().getRegistrationId();
+                String providerId = String.valueOf(kakaoResponse.id());
+                String username = registrationId + "_" + providerId;
+                String dummyPassword = passwordEncoder.encode("{bcrypt}" + UUID.randomUUID());
+
+                return userAccountService.searchUser(username)
+                        .map(BoardPrincipal::from)
+                        .orElseGet(() ->
+                                BoardPrincipal.from(
+                                        userAccountService.saveUser(
+                                                username,
+                                                dummyPassword,
+                                                kakaoResponse.email(),
+                                                kakaoResponse.nickname(),
+                                                null
+                                        )
+                                )
+                        );
+            };
+
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        }
+
     }
-
-    @Bean
-    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
-        return username -> userAccountRepository
-                .findById(username)  // 데이터베이스에서 사용자 ID(username)로 사용자 정보를 찾음
-                .map(UserAccountDto::from)  // 찾은 정보를 UserAccountDto로 변환
-                .map(BoardPrincipal::from)  // UserAccountDto를 BoardPrincipal로 변환 (Spring Security가 사용하는 사용자 객체)
-                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));  // 사용자가 없으면 예외 발생
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-}
